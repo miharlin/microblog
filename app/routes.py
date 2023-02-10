6231897ebebdbc82e0c7bad5d1c0bd751f52d963
Mia Harlin <miharlin@packer.edu>
2023-02-10 09:32:06 -0500

    class challenge: songs

--- app/routes.py
+++ app/routes.py
@@ -16,3 +16,18 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/songs')
+def songs():
+    user = {'username': 'Mia'}
+    songs = [
+        {
+            'title': 'let it be',
+            'artist': 'Beetles'
+        },
+        {
+            'title': 'billy jean',
+            'artist': 'Micheal Jackson'
+        }
+    ]
+    return render_template('songs.html', title='songs', user=user, songs=songs)
